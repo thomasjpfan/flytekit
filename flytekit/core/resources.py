e828ceed55719f453f683b17e99f10d6ce3a331a
Thomas J. Fan <thomasjpfan@gmail.com>
2025-02-07 10:28:48 -0500

    Do not import kubernetes in flytekit core
    Signed-off-by: Thomas J. Fan <thomasjpfan@gmail.com>

--- flytekit/core/resources.py
+++ flytekit/core/resources.py
@@ -1,8 +1,12 @@
+import typing
 from dataclasses import dataclass, fields
 from typing import List, Optional, Union
 
-from kubernetes.client import V1Container, V1PodSpec, V1ResourceRequirements
 from mashumaro.mixins.json import DataClassJSONMixin
+
+if typing.TYPE_CHECKING:
+    from kubernetes.client import V1PodSpec
+
 
 from flytekit.models import task as task_models
 
@@ -108,6 +112,8 @@ def pod_spec_from_resources(
     limits: Optional[Resources] = None,
     k8s_gpu_resource_key: str = "nvidia.com/gpu",
 ) -> V1PodSpec:
+    from kubernetes.client import V1Container, V1PodSpec, V1ResourceRequirements
+
     def _construct_k8s_pods_resources(resources: Optional[Resources], k8s_gpu_resource_key: str):
         if resources is None:
             return None
